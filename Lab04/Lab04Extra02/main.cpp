40de56c4319ec3c41f485ab17b611c67a0a939b6
Dominique Russell <30952031+whatever9999@users.noreply.github.com>
2021-10-24 23:41:42 +0100

    Try merging stuff

---
+++ Lab04/Lab04Extra02/main.cpp
@@ -0,0 +1,475 @@
+#include <d3d11.h>
+#include <dxgi.h>
+#include <d3dx11.h>
+#include <windows.h>
+#include <dxerr.h>
+
+#define _XM_NO_INTRINSICS_
+#define XM_NO_ALIGNMENT
+#include <DirectXMath.h>
+
+using namespace DirectX;
+
+// Global Vars
+HINSTANCE	g_hInst = NULL;
+HWND		g_hWnd	= NULL;
+
+D3D_DRIVER_TYPE			g_driverType			= D3D_DRIVER_TYPE_NULL;
+D3D_FEATURE_LEVEL		g_featureLevel			= D3D_FEATURE_LEVEL_11_0;
+ID3D11Device*			g_pD3DDevice			= NULL;
+ID3D11DeviceContext*	g_pImmediateContext		= NULL;
+IDXGISwapChain*			g_pSwapChain			= NULL;
+ID3D11RenderTargetView* g_pBackBufferRTView		= NULL;
+ID3D11Buffer*			g_pVertexBuffer;
+ID3D11VertexShader*		g_pVertexShader;
+ID3D11PixelShader*		g_pPixelShader;
+ID3D11InputLayout*		g_pInputLayout;
+ID3D11Buffer*			g_pConstantBuffer0;
+
+struct POS_COL_VERTEX
+{
+	XMFLOAT3 pos;
+	XMFLOAT4 Col;
+};
+
+struct CONSTANT_BUFFER0
+{
+	XMFLOAT3 PosChange;
+	float Scale;
+	int Choice;
+	XMFLOAT4 Colour_1;
+	XMFLOAT4 Colour_2;
+	XMFLOAT4 Colour_3;
+	XMFLOAT4 Colour_4;
+};
+CONSTANT_BUFFER0 g_cb0_values;
+
+
+// Window Title
+char		g_TutorialName[100] = "Lab 03 Exercise 01\0";
+
+// Forward Declarations
+HRESULT InitialiseWindow(HINSTANCE hInstance, int nCmdShow);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HRESULT InitialiseD3D();
+void	ShutdownD3D();
+void	RenderFrame(void);
+void	ResizeWindow(UINT new_size[2]);
+HRESULT InitialiseGraphics(void);
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	if (FAILED(InitialiseWindow(hInstance, nCmdShow)))
+	{
+		DXTRACE_MSG("Failed to create Window");
+		return 0;
+	}
+
+	MSG msg = { 0 };
+
+	if (FAILED(InitialiseD3D()))
+	{
+		DXTRACE_MSG("Failed to create Device");
+		return 0;
+	}
+
+	if (FAILED(InitialiseGraphics()))
+	{
+		DXTRACE_MSG("Failed to initialise graphics");
+		return 0;
+	}
+
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+			RenderFrame();
+		}
+	}
+
+	ShutdownD3D();
+
+	return (int)msg.wParam;
+}
+
+HRESULT InitialiseWindow(HINSTANCE hInstance, int nCmdShow)
+{
+	char Name[100] = "Dominique Russell\0";
+
+	WNDCLASSEX wcex = { 0 };
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.hInstance = hInstance;
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	// wcex.hbrBackground = (HBRUSH)(COLOUR_WINDOW + 1);
+	wcex.lpszClassName = Name;
+
+	if (!RegisterClassEx(&wcex)) return E_FAIL;
+
+	g_hInst = hInstance;
+	RECT rc = { 0, 0, 640, 480 };
+	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+	g_hWnd = CreateWindow(Name, g_TutorialName, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, hInstance, NULL);
+
+	if (!g_hWnd)
+	{
+		return E_FAIL;
+	}
+
+	ShowWindow(g_hWnd, nCmdShow);
+
+	return S_OK;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch (message)
+	{
+		case WM_CHAR:
+			switch (wParam)
+			{
+				case 'w':
+					g_cb0_values.PosChange.y += 1;
+					break;
+				case 's':
+					g_cb0_values.PosChange.y -= 1;
+					break;
+				case 'a':
+					g_cb0_values.PosChange.x -= 1;
+					break;
+				case 'd':
+					g_cb0_values.PosChange.x += 1;
+					break;
+				case 'z':
+					g_cb0_values.Scale -= 0.1;
+					break;
+				case 'x':
+					g_cb0_values.Scale += 0.1;
+					break;
+				case '1':
+					g_cb0_values.Choice = 1;
+					break;
+				case '2':
+					g_cb0_values.Choice = 2;
+					break;
+				case '3':
+					g_cb0_values.Choice = 3;
+					break;
+				case '4':
+					g_cb0_values.Choice = 4;
+					break;
+			}
+
+			g_pImmediateContext->UpdateSubresource(g_pConstantBuffer0, 0, 0, &g_cb0_values, 0, 0);
+			break;
+		case WM_SIZE:
+			if (g_pSwapChain)
+			{
+				UINT new_size[2];
+				new_size[0] = LOWORD(lParam);
+				new_size[1] = HIWORD(lParam);
+
+				ResizeWindow(new_size);
+			}
+			break;
+		case WM_PAINT:
+			hdc = BeginPaint(hWnd, &ps);
+			EndPaint(hWnd, &ps);
+			break;
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
+		case WM_KEYDOWN:
+			if (wParam == VK_ESCAPE)
+			{
+				DestroyWindow(g_hWnd);
+				return 0;
+			}
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+
+	return 0;
+}
+
+HRESULT InitialiseD3D()
+{
+	HRESULT hr = S_OK;
+
+	RECT rc;
+	GetClientRect(g_hWnd, &rc);
+	UINT width = rc.right - rc.left;
+	UINT height = rc.bottom - rc.top;
+
+	UINT createDeviceFlags = 0;
+
+#ifdef _DEBUG
+	createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif // _DEBUG
+
+	D3D_DRIVER_TYPE driverTypes[] =
+	{
+		D3D_DRIVER_TYPE_HARDWARE,
+		D3D_DRIVER_TYPE_WARP,
+		D3D_DRIVER_TYPE_REFERENCE,
+	};
+	UINT numDriverTypes = ARRAYSIZE(driverTypes);
+
+	D3D_FEATURE_LEVEL featureLevels[] =
+	{
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_1,
+		D3D_FEATURE_LEVEL_10_0,
+	};
+	UINT numFeatureLevels = ARRAYSIZE(featureLevels);
+
+	DXGI_SWAP_CHAIN_DESC sd;
+	ZeroMemory(&sd, sizeof(sd));
+	sd.BufferCount = 1;
+	sd.BufferDesc.Width = width;
+	sd.BufferDesc.Height = height;
+	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	sd.BufferDesc.RefreshRate.Numerator = 60;
+	sd.BufferDesc.RefreshRate.Denominator = 1;
+	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	sd.OutputWindow = g_hWnd;
+	sd.SampleDesc.Count = 1;
+	sd.SampleDesc.Quality = 0;
+	sd.Windowed = true;
+
+	for (UINT driverTypeIndex = 0; driverTypeIndex < numDriverTypes; driverTypeIndex++)
+	{
+		g_driverType = driverTypes[driverTypeIndex];
+		hr = D3D11CreateDeviceAndSwapChain(	NULL, g_driverType, NULL, createDeviceFlags, featureLevels, numFeatureLevels,
+											D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pD3DDevice, &g_featureLevel, &g_pImmediateContext	);
+		if (SUCCEEDED(hr))
+		{
+			break;
+		}
+	}
+
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+
+	ID3D11Texture2D* pBackBufferTexture;
+	hr = g_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBufferTexture);
+
+	if (FAILED(hr)) return hr;
+
+	hr = g_pD3DDevice->CreateRenderTargetView(pBackBufferTexture, NULL, &g_pBackBufferRTView);
+	pBackBufferTexture->Release();
+
+	if (FAILED(hr)) return hr;
+
+	g_pImmediateContext->OMSetRenderTargets(1, &g_pBackBufferRTView, NULL);
+
+	D3D11_VIEWPORT viewport;
+
+	viewport.TopLeftX = 0;
+	viewport.TopLeftY = 0;
+	viewport.Width = (FLOAT)width;
+	viewport.Height = (FLOAT)height;
+	viewport.MinDepth = 0.0f;
+	viewport.MaxDepth = 1.0f;
+
+	g_pImmediateContext->RSSetViewports(1, &viewport);
+
+	return S_OK;
+}
+
+void ShutdownD3D()
+{
+	if (g_pConstantBuffer0)		g_pConstantBuffer0->Release();
+	if (g_pVertexBuffer)		g_pVertexBuffer->Release();
+	if (g_pInputLayout)			g_pInputLayout->Release();
+	if (g_pVertexShader)		g_pVertexShader->Release();
+	if (g_pPixelShader)			g_pPixelShader->Release();
+	if (g_pBackBufferRTView)	g_pBackBufferRTView->Release();
+	if (g_pSwapChain)			g_pSwapChain->Release();
+	if (g_pImmediateContext)	g_pImmediateContext->Release();
+	if (g_pD3DDevice)			g_pD3DDevice->Release();
+}
+
+HRESULT InitialiseGraphics()
+{
+	HRESULT hr = S_OK;
+
+	POS_COL_VERTEX vertices[] =
+	{
+		{XMFLOAT3(0.9f, 0.9f, 0.0f), XMFLOAT4(0.4f, 0.1f, 0.1f, 1.0f)},
+		{XMFLOAT3(0.9f, -0.9f, 0.0f), XMFLOAT4(0.7f, 0.3f, 0.3f, 1.0f)},
+		{XMFLOAT3(-0.9f, -0.9f, 0.0f), XMFLOAT4(0.9f, 0.5f, 0.5f, 1.0f)},
+	};
+
+	// Create Vertex Buffer
+	D3D11_BUFFER_DESC bufferDesc;
+	ZeroMemory(&bufferDesc, sizeof(bufferDesc));
+	bufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+	bufferDesc.ByteWidth = sizeof(POS_COL_VERTEX) * 3;
+	bufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	hr = g_pD3DDevice->CreateBuffer(&bufferDesc, NULL, &g_pVertexBuffer);
+
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+
+	// Create Constant Buffer
+	D3D11_BUFFER_DESC constant_buffer_desc;
+	ZeroMemory(&constant_buffer_desc, sizeof(constant_buffer_desc));
+	constant_buffer_desc.Usage = D3D11_USAGE_DEFAULT;
+	constant_buffer_desc.ByteWidth = 96;
+	constant_buffer_desc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	hr = g_pD3DDevice->CreateBuffer(&constant_buffer_desc, NULL, &g_pConstantBuffer0);
+
+	// Attach cb0_values
+	g_cb0_values.Scale = 1;
+	g_cb0_values.Choice = 1;
+	g_cb0_values.Colour_1 = XMFLOAT4(0.1f, 0.1f, 0.1f, 1.0f);
+	g_cb0_values.Colour_2 = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+	g_cb0_values.Colour_3 = XMFLOAT4(0.2f, 0.4f, 0.6f, 1.0f);
+	g_cb0_values.Colour_4 = XMFLOAT4(0.8f, 0.0f, 0.8f, 1.0f);
+	g_pImmediateContext->UpdateSubresource(g_pConstantBuffer0, 0, 0, &g_cb0_values, 0, 0);
+	g_pImmediateContext->VSSetConstantBuffers(0, 1, &g_pConstantBuffer0);
+
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+
+	D3D11_MAPPED_SUBRESOURCE ms;
+
+	g_pImmediateContext->Map(g_pVertexBuffer, NULL, D3D11_MAP_WRITE_DISCARD, NULL, &ms);
+
+	memcpy(ms.pData, vertices, sizeof(vertices));
+
+	g_pImmediateContext->Unmap(g_pVertexBuffer, NULL);
+
+	ID3DBlob* VS, * PS, * error;
+
+	hr = D3DX11CompileFromFile("shaders.hlsl", 0, 0, "VShader", "vs_4_0", 0, 0, 0, &VS, &error, 0);
+
+	if (error != 0)
+	{
+		OutputDebugStringA((char*)error->GetBufferPointer());
+		error->Release();
+		if (FAILED(hr))
+		{
+			return hr;
+		}
+	}
+
+	hr = D3DX11CompileFromFile("shaders.hlsl", 0, 0, "PShader", "ps_4_0", 0, 0, 0, &PS, &error, 0);
+
+	if (error != 0)
+	{
+		OutputDebugStringA((char*)error->GetBufferPointer());
+		error->Release();
+		if (FAILED(hr))
+		{
+			return hr;
+		}
+	}
+
+	hr = g_pD3DDevice->CreateVertexShader(VS->GetBufferPointer(), VS->GetBufferSize(), NULL, &g_pVertexShader);
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+
+	hr = g_pD3DDevice->CreatePixelShader(PS->GetBufferPointer(), PS->GetBufferSize(), NULL, &g_pPixelShader);
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+
+	g_pImmediateContext->VSSetShader(g_pVertexShader, 0, 0);
+	g_pImmediateContext->PSSetShader(g_pPixelShader, 0, 0);
+
+	D3D11_INPUT_ELEMENT_DESC iedesc[] =
+	{
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0},
+	};
+
+	hr = g_pD3DDevice->CreateInputLayout(iedesc, 2, VS->GetBufferPointer(), VS->GetBufferSize(), &g_pInputLayout);
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+
+	g_pImmediateContext->IASetInputLayout(g_pInputLayout);
+
+	return S_OK;
+}
+
+void RenderFrame(void)
+{
+	float rgba_clear_colour[4] = { 0.1f, 0.2f, 0.6f, 1.0f };
+	g_pImmediateContext->ClearRenderTargetView(g_pBackBufferRTView, rgba_clear_colour);
+
+	UINT stride = sizeof(POS_COL_VERTEX);
+	UINT offset = 0;
+	g_pImmediateContext->IASetVertexBuffers(0, 1, &g_pVertexBuffer, &stride, &offset);
+
+	g_pImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	g_pImmediateContext->Draw(3, 0);
+
+	g_pSwapChain->Present(0, 0);
+}
+
+void ResizeWindow(UINT new_size[2])
+{
+	HRESULT hr;
+
+	g_pImmediateContext->OMSetRenderTargets(1, &g_pBackBufferRTView, NULL);
+
+	// Release all outstanding references to the swap chain's buffers.
+	g_pBackBufferRTView->Release();
+
+	// Preserve the existing buffer count and format.
+	// Automatically choose the width and height to match the client rect for HWNDs.
+	hr = g_pSwapChain->ResizeBuffers(1, new_size[0], new_size[1], DXGI_FORMAT_R8G8B8A8_UNORM, 0);
+
+	if (FAILED(hr)) abort();
+
+	// Get buffer and create a render-target-view.
+	ID3D11Texture2D* pBackBufferTexture;
+	hr = g_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBufferTexture);
+
+	if (FAILED(hr)) abort();
+
+	hr = g_pD3DDevice->CreateRenderTargetView(pBackBufferTexture, NULL, &g_pBackBufferRTView);
+	
+	if (FAILED(hr)) abort();
+
+	pBackBufferTexture->Release();
+
+	g_pImmediateContext->OMSetRenderTargets(1, &g_pBackBufferRTView, NULL);
+
+	D3D11_VIEWPORT viewport;
+	viewport.TopLeftX = 0;
+	viewport.TopLeftY = 0;
+	viewport.Width = (FLOAT)new_size[0];
+	viewport.Height = (FLOAT)new_size[1];
+	viewport.MinDepth = 0.0f;
+	viewport.MaxDepth = 1.0f;
+	g_pImmediateContext->RSSetViewports(1, &viewport);
+}
