1f2208e995b3587b5ebd7c93db2bb2e04f7bb959
whatever9999 <30952031+whatever9999@users.noreply.github.com>
2021-11-25 10:01:00 +0000

    Make model look at and follow camera

---
+++ Tutorial11/Tutorial11Exercise01/model.cpp
@@ -0,0 +1,193 @@
+#include "model.h"
+
+struct MODEL_CONSTANT_BUFFER // 160 bytes
+{
+	XMMATRIX WorldViewProjection;
+	XMVECTOR directional_light_vector;
+	XMVECTOR directional_light_colour;
+	XMVECTOR ambient_light_colour;
+	XMVECTOR point_light_position;
+	XMVECTOR point_light_colour;
+	XMFLOAT3 point_light_attenuation;
+	float packing_1;
+};
+
+
+HRESULT Model::Setup()
+{
+	// Shaders
+	HRESULT hr = S_OK;
+	ID3DBlob* VS, * PS, * error;
+
+	hr = D3DX11CompileFromFile("model_shaders.hlsl", 0, 0, "ModelVS", "vs_4_0", 0, 0, 0, &VS, &error, 0);
+
+	if (error != 0)
+	{
+		OutputDebugStringA((char*)error->GetBufferPointer());
+		error->Release();
+		if (FAILED(hr))
+		{
+			return hr;
+		}
+	}
+
+	hr = D3DX11CompileFromFile("model_shaders.hlsl", 0, 0, m_pixel_shader, "ps_4_0", 0, 0, 0, &PS, &error, 0);
+
+	if (error != 0)
+	{
+		OutputDebugStringA((char*)error->GetBufferPointer());
+		error->Release();
+		if (FAILED(hr))
+		{
+			return hr;
+		}
+	}
+
+	hr = m_D3DDevice->CreateVertexShader(VS->GetBufferPointer(), VS->GetBufferSize(), NULL, &m_pVShader);
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+
+	hr = m_D3DDevice->CreatePixelShader(PS->GetBufferPointer(), PS->GetBufferSize(), NULL, &m_pPShader);
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+
+	// Input Layout
+	D3D11_INPUT_ELEMENT_DESC iedesc[] =
+	{
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0},
+	};
+
+	hr = m_D3DDevice->CreateInputLayout(iedesc, ARRAYSIZE(iedesc), VS->GetBufferPointer(), VS->GetBufferSize(), &m_pInputLayout);
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+
+	// Constant Buffer
+	D3D11_BUFFER_DESC constant_buffer_desc;
+	ZeroMemory(&constant_buffer_desc, sizeof(constant_buffer_desc));
+	constant_buffer_desc.Usage = D3D11_USAGE_DEFAULT;
+	constant_buffer_desc.ByteWidth = 160;
+	constant_buffer_desc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	hr = m_D3DDevice->CreateBuffer(&constant_buffer_desc, NULL, &m_pConstantBuffer);
+
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+
+	// Setup Sampler
+	D3D11_SAMPLER_DESC sampler_desc;
+	ZeroMemory(&sampler_desc, sizeof(sampler_desc));
+	sampler_desc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	sampler_desc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+	sampler_desc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+	sampler_desc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+	sampler_desc.MaxLOD = D3D11_FLOAT32_MAX;
+	m_D3DDevice->CreateSamplerState(&sampler_desc, &m_pSampler0);
+}
+
+void Model::LookAt_XZ(float x, float z)
+{
+	float dx = x - m_x;
+	float dz = z - m_z;
+
+	m_yAngle = atan2(dx, dz) * (180.0 / XM_PI);
+}
+
+void Model::MoveForward(float distance)
+{
+	m_x += sin(m_yAngle * (XM_PI / 180.0)) * distance;
+	m_z += cos(m_yAngle * (XM_PI / 180.0)) * distance;
+}
+
+HRESULT Model::LoadObjModel(char* filename, char* pixel_shader, char* vertex_shader)
+{
+	m_pixel_shader = pixel_shader;
+	m_vertex_shader = vertex_shader;
+
+	HRESULT hr = S_OK;
+
+	m_pObject = new ObjFileModel(filename, m_D3DDevice, m_pImmediateContext);
+	if (m_pObject->filename == "FILE NOT LOADED")
+	{
+		return S_FALSE;
+	}
+
+	hr = Setup();
+
+	return hr;
+}
+
+void Model::AddTextures(char* texture0_filename, char* texture1_filename)
+{
+	m_pImmediateContext->PSSetShaderResources(0, 1, &m_pTexture0);
+	D3DX11CreateShaderResourceViewFromFile(m_D3DDevice, texture0_filename, NULL, NULL, &m_pTexture0, NULL);
+	D3DX11CreateShaderResourceViewFromFile(m_D3DDevice, texture1_filename, NULL, NULL, &m_pTexture1, NULL);
+}
+
+void Model::AddDirectionalLight(XMVECTOR directional_light_shines_from, XMVECTOR directional_light_colour, XMMATRIX rotate_directional_light)
+{
+	m_directional_light_shines_from = directional_light_shines_from;
+	m_directional_light_colour = directional_light_colour;
+	m_rotate_directional_light = rotate_directional_light;
+}
+void Model::AddAmbientLight(XMVECTOR ambient_light_colour)
+{
+	m_ambient_light_colour = ambient_light_colour;
+}
+void Model::AddPointLight(XMVECTOR point_light_position, XMVECTOR point_light_colour, XMFLOAT3 point_light_attenuation)
+{
+	m_point_light_position = point_light_position;
+	m_point_light_colour = point_light_colour;
+	m_point_light_attenuation = point_light_attenuation;
+}
+
+void Model::Draw(XMMATRIX* view, XMMATRIX* projection)
+{
+	XMMATRIX world = XMMatrixScaling(m_scale, m_scale, m_scale);
+	world *= XMMatrixRotationX(XMConvertToRadians(m_xAngle));
+	world *= XMMatrixRotationY(XMConvertToRadians(m_yAngle));
+	world *= XMMatrixRotationZ(XMConvertToRadians(m_zAngle));
+	world *= XMMatrixTranslation(m_x, m_y, m_z);
+
+	MODEL_CONSTANT_BUFFER model_cb_values;
+	model_cb_values.WorldViewProjection = world * (*view) * (*projection);
+
+	// Lighting colours
+	model_cb_values.point_light_colour = m_point_light_colour;
+	model_cb_values.directional_light_colour = m_directional_light_colour;
+	model_cb_values.ambient_light_colour = m_ambient_light_colour;
+	model_cb_values.point_light_attenuation = m_point_light_attenuation;
+
+	// Lighting positions
+	XMMATRIX transpose = XMMatrixTranspose(world);
+	XMVECTOR determinant;
+	XMMATRIX inverse = XMMatrixInverse(&determinant, world);
+	model_cb_values.directional_light_vector = XMVector3Transform(XMVector3Transform(m_directional_light_shines_from, m_rotate_directional_light), transpose);
+	model_cb_values.directional_light_vector = XMVector3Normalize(model_cb_values.directional_light_vector);
+	model_cb_values.point_light_position = XMVector3Transform(m_point_light_position, inverse);
+
+	m_pImmediateContext->VSSetConstantBuffers(0, 1, &m_pConstantBuffer);
+	m_pImmediateContext->UpdateSubresource(m_pConstantBuffer, 0, 0, &model_cb_values, 0, 0);
+
+	m_pImmediateContext->VSSetShader(m_pVShader, 0, 0);
+	m_pImmediateContext->PSSetShader(m_pPShader, 0, 0);
+	m_pImmediateContext->IASetInputLayout(m_pInputLayout);
+
+	if (m_pTexture0 && m_pTexture1)
+	{
+		m_pImmediateContext->PSSetSamplers(0, 1, &m_pSampler0);
+		m_pImmediateContext->PSSetShaderResources(0, 1, &m_pTexture0);
+		m_pImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+		m_pImmediateContext->PSSetShaderResources(1, 1, &m_pTexture1);
+	}
+
+	m_pObject->Draw();
+}
---
+++ Tutorial11/Tutorial11Exercise01/model.h
@@ -0,0 +1,121 @@
+#pragma once
+
+#include <d3d11.h>
+#include <math.h>
+
+#include "objfilemodel.h"
+
+#define _XM_NO_INTRINSICS_
+#define XM_NO_ALIGNMENT
+#include <DirectXMath.h>
+
+using namespace DirectX;
+
+class Model
+{
+private:
+	ID3D11Device*			m_D3DDevice;
+	ID3D11DeviceContext*	m_pImmediateContext;
+
+	ObjFileModel*				m_pObject;
+	ID3D11VertexShader*			m_pVShader;
+	ID3D11PixelShader*			m_pPShader;
+	ID3D11InputLayout*			m_pInputLayout;
+	ID3D11Buffer*				m_pConstantBuffer;
+	ID3D11ShaderResourceView*	m_pTexture0;
+	ID3D11ShaderResourceView*	m_pTexture1;
+	ID3D11SamplerState*			m_pSampler0;
+
+	XMVECTOR	m_directional_light_shines_from;
+	XMVECTOR	m_directional_light_colour;
+	XMVECTOR	m_ambient_light_colour;
+	XMMATRIX	m_rotate_directional_light;
+	XMVECTOR	m_point_light_position;
+	XMVECTOR	m_point_light_colour;
+	XMFLOAT3	m_point_light_attenuation;
+
+	char* m_vertex_shader;
+	char* m_pixel_shader;
+
+	float m_x, m_y, m_z;
+	float m_xAngle, m_yAngle, m_zAngle;
+	float m_scale;
+
+	HRESULT Setup();
+
+public:
+	Model(ID3D11Device* device, ID3D11DeviceContext* deviceContext)
+	{
+		m_D3DDevice = device;
+		m_pImmediateContext = deviceContext;
+
+		m_pObject = nullptr;
+		m_pVShader = nullptr;
+		m_pPShader = nullptr;
+		m_pInputLayout = nullptr;
+		m_pConstantBuffer = nullptr;
+
+		m_x = 0.0f; 
+		m_y = 0.0f;
+		m_z = 0.0f;
+		m_xAngle = 0.0f; 
+		m_yAngle = 0.0f; 
+		m_zAngle = 0.0f;
+		m_scale = 1.0f;
+	}
+	~Model()
+	{
+		if (m_pObject)
+		{
+			delete m_pObject;
+			m_pObject = nullptr;
+		}
+
+		if (m_pTexture0)		m_pTexture0->Release();
+		if (m_pTexture1)		m_pTexture1->Release();
+		if (m_pSampler0)		m_pSampler0->Release();
+		if (m_pConstantBuffer)	m_pConstantBuffer->Release();
+		if (m_pVShader)			m_pVShader->Release();
+		if (m_pInputLayout)		m_pInputLayout->Release();
+		if (m_pPShader)			m_pPShader->Release();
+	}
+
+	// Setters
+	void SetX(float x) { m_x = x; }
+	void SetY(float y) { m_y = y; }
+	void SetZ(float z) { m_z = z; }
+	void SetXAngle(float x_angle) { m_xAngle = x_angle; }
+	void SetYAngle(float y_angle) { m_yAngle = y_angle; }
+	void SetZAngle(float z_angle) { m_zAngle = z_angle; }
+	void SetScale(float scale) { m_scale = scale; }
+	// Getters
+	float GetX() { return m_x; }
+	float GetY() { return m_y; }
+	float GetZ() { return m_z; }
+	float GetXAngle() { return m_xAngle; }
+	float GetYAngle() { return m_yAngle; }
+	float GetZAngle() { return m_zAngle; }
+	float GetScale() { return m_scale; }
+	// Incrementors
+	float IncX(float amount) { m_x += amount; }
+	float IncY(float amount) { m_y += amount; }
+	float IncZ(float amount) { m_z += amount; }
+	float IncXAngle(float amount) { m_xAngle += amount; }
+	float IncYAngle(float amount) { m_yAngle += amount; }
+	float IncZAngle(float amount) { m_zAngle += amount; }
+	float IncScale(float amount) { m_scale += amount; }
+
+	// Orienting
+	void LookAt_XZ(float x, float z);
+	void MoveForward(float distance);
+
+	HRESULT LoadObjModel(char* filename, char* pixel_shader, char* vertex_shader);
+
+	void AddTextures(char* texture0_filename, char* texture1_filename);
+
+	void AddDirectionalLight(XMVECTOR directional_light_shines_from, XMVECTOR directional_light_colour, XMMATRIX rotate_directional_light);
+	void AddAmbientLight(XMVECTOR ambient_light_colour);
+	void AddPointLight(XMVECTOR point_light_position, XMVECTOR point_light_colour, XMFLOAT3 point_light_attenuation);
+
+	void Draw(XMMATRIX* view, XMMATRIX* projection);
+};
